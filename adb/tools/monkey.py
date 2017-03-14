e711b959654e989b7dc4f39240c084cd184ff6c3
guoliangyong@xiaomi.com <guoliangyong>
2017-03-14 11:19:25 +0800

    fix bug: 路径更改&monkey执行后等待

--- adb/tools/monkey.py
+++ adb/tools/monkey.py
@@ -25,31 +25,34 @@ class Monkey(object):
             raise Exception("No device found")
 
         for device in devices:
-           self.adb.logger.debug("Device Id: %s Run monkey script:")
+           self.adb.logger.debug("Device Id: %s Run monkey script:" % device['uuid'])
            self.adb.setDeviceId(device['uuid'])
-           self.adb.shell("monkey " + args, wait=False)
+           result,_ = self.adb.shell("monkey " + args, wait=False)
+           print("start monkey %s "  % result)
         #获取统计数据
         cpu_data = []
         mem_data = []
+        time.sleep(3)
         while(True):
             #检查monkey是否结束
-            pid = self.adb.getPIDByName("com.android.commands.monke")
+            pid = self.adb.getPIDByName('com.android.commands.monkey')
             if len(pid)<=0:
+                print "no monkey start now.stop."
                 break
             result1, cpu = self.adb.getCpuUsage()
-            result2, mem = self.adb.getMemTotal()
+            result2, mem = self.adb.getMemFree()
             if not result1 or not result2:
                 continue
             mem = float(mem.split()[0])
             cpu_data.append(cpu)
             mem_data.append(mem)
         #输出为html report
-        template_file = os.path.dirname(os.path.abspath(__file__))+"/../../template/performance.html"
+        template_file = os.path.dirname(os.path.abspath(__file__))+"/../template/performance.html"
         f = open(template_file)
         template_str = f.read()
         template = Template(template_str)
         out = template.render({"cpuinfo": cpu_data, "meminfo": mem_data})
-        dest_file = os.path.dirname(os.path.abspath(__file__))+"/../../output/" + str(time.time()) + "_monkey_performance.html"
+        dest_file = os.path.dirname(os.path.abspath(__file__))+"/../output/" + str(time.time()) + "_monkey_performance.html"
         f = open(dest_file, 'w')
         f.write(out)
 
@@ -60,5 +63,5 @@ if __name__ == "__main__":
     if len(monkey_argv)>1:
         arg = monkey_argv[1]
     else:
-        arg = '-v -v -v -p com.example.android.testing.uiautomator.BasicSample --pct-syskeys 0 --pct-motion 0 --throttle 300 --bugreport 1000'
+        arg = '-v -v -v -p com.example.android.testing.uiautomator.BasicSample --pct-syskeys 0 --pct-motion 0 --throttle 300 --bugreport 100'
     monkeytool.run(arg)
